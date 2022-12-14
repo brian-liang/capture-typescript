bb8c35a5e1acc9a313a3a829ffceeb11943c10cf
brian-liang <brian.j.liang@gmail.com>
2022-12-13 21:50:44 -0700

    09 - React Router for the home, contactUs and ourWork pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
         <GlobalStyle/>
         <Nav/>
-        <AboutUs/>
+        <Routes>
+            <Route path="/" element={<AboutUs/>}></Route>
+            <Route path="work" element={<OurWork/>}></Route>
+            <Route path="contact" element={<ContactUs/>}></Route>
+        </Routes>
     </div>
   );
 }
