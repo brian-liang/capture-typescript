dd8555b974fdb35224d6be258ea93bf0a9027ee3
brian-liang <brian.j.liang@gmail.com>
2022-12-14 10:36:15 -0700

    10 - Working on adding link to the OurWork Page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,7 +13,7 @@ const Nav = () => {
                     <Link to="work">2. Our Work</Link>
                 </li>
                 <li>
-                    <Link to="#contact">3. Contact Us</Link>
+                    <Link to="contact">3. Contact Us</Link>
                 </li>
             </ul>
         </StyledNav>
--- src/pages/OurWork.tsx
+++ src/pages/OurWork.tsx
@@ -1,9 +1,59 @@
+import { timeStamp } from "console"
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import athlete from './../img/athlete2.png'
+import racer from '../img/the-racer2.jpg'
+import goodtimes from '../img/good-times2.jpg'
+
 const OurWork = () => {
     return(
-        <div>
-            <h1>Our Work</h1>
-        </div>
+        <Work>
+            <Movie>
+                <h2>The Athlete</h2>
+                <div className="line"></div>
+                <Link to='/'>
+                    <img src={athlete} alt="athlete"/>
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link to='/'>
+                    <img src={racer} alt="racer"/>
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                <Link to='/'>
+                    <img src={goodtimes} alt="goodtimes"/>
+                </Link>
+            </Movie>
+        </Work>
     )
 }
+
+const Work = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+    background: white;
+    h2{
+        padding: 1rem 0 rem;
+    }
+`
+const Movie = styled.div`
+    padding-bottom: 10rem;
+    .line{
+        height: 0.5rem;
+        backround: #cccccc;
+        margin-bottom: 3rem;
+    }
+    img{
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+`
 
 export default OurWork
